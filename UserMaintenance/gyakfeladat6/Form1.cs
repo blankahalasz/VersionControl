2646a85f36f8e4fc8e1ba390209c84f76616449c
Blanka Halász <blanka.halasz@stud.uni-corvinus.hu>
2020-10-18 20:16:27 +0200

    xml feldolgozas

--- UserMaintenance/gyakfeladat6/Form1.cs
+++ UserMaintenance/gyakfeladat6/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace gyakfeladat6
 {
@@ -21,9 +22,29 @@ namespace gyakfeladat6
             GetExchangeRates();
             dataGridView1.DataSource = Rates;
 
+            Xmlprocess();
+
         }
 
-        
+        private void Xmlprocess()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+
+            }
+        }
+
         private static void GetExchangeRates()
         {
             var MnbService = new MNBArfolyamServiceSoapClient();
@@ -38,6 +59,11 @@ namespace gyakfeladat6
 
             var response = MnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            
+
         }
+
+        
     }
 }
