debcea1d703dc0236c1517ea0dfffb453cdc4da9
Blanka Halász <blanka.halasz@stud.uni-corvinus.hu>
2020-11-01 21:15:19 +0100

    osztalyok letrehozasa

---
+++ UserMaintenance/gyakfeladat7/Entities/Birthprobability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakfeladat7.Entities
+{
+    class Birthprobability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+
+        public double Probability { get; set; }
+    }
+}
---
+++ UserMaintenance/gyakfeladat7/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakfeladat7.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int BirthYear { get; set; }
+        public double Probability { get; set; }
+    }
+}
