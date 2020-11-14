c9454d81c8aeb0f264de36b386410bafbd6a42e9
Blanka Halász <blanka.halasz@stud.uni-corvinus.hu>
2020-11-14 16:42:36 +0100

    labdagyar letrehozasa

---
+++ UserMaintenance/gyak_8_gwsb1a/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak_8_gwsb1a.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
