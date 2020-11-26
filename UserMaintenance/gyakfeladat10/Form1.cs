982841998488e60b539eece81c0fb015d7eff293
Blanka Halász <blanka.halasz@stud.uni-corvinus.hu>
2020-11-26 20:44:25 +0100

    iteracios lepes felepitese

--- UserMaintenance/gyakfeladat10/Form1.cs
+++ UserMaintenance/gyakfeladat10/Form1.cs
@@ -27,9 +27,24 @@ namespace gyakfeladat10
             this.Controls.Add(ga);
             // gc.AddPlayer();
             // gc.Start(true);
-            gc.AddPlayer(nbrOfSteps);
+
+            gc.GameOver += Gc_GameOver;
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
             gc.Start(true);
            
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            {
+                generation++;
+                label1.Text = string.Format(
+                    "{0}. generáció",
+                    generation);
+            }
         }
     }
 }
