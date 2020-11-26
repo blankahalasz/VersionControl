ad4106cbdb9e3ccfde4442f68c1e84671e797c06
Blanka Halász <blanka.halasz@stud.uni-corvinus.hu>
2020-11-26 20:55:44 +0100

    gyoztes kivalasztasa

--- UserMaintenance/gyakfeladat10/Form1.cs
+++ UserMaintenance/gyakfeladat10/Form1.cs
@@ -19,6 +19,7 @@ namespace gyakfeladat10
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        Brain winnerBrain = null;
 
         public Form1()
         {
@@ -48,6 +49,16 @@ namespace gyakfeladat10
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
 
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
